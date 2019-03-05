29ad274f0f47895a01b174cb85a46d32d3cc4439
Beviz Elek <cvincez@vipmail.hu>
2019-03-05 09:18:31 +0100

    Masodik feltöltés

---
+++ atom_hello.py
@@ -0,0 +1,6 @@
+def hi(name):
+    print('Hello ' + name + '!')
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Következő lány:')
---
+++ hello.py
@@ -0,0 +1,1 @@
+print("Hello UJ Vilag!")
